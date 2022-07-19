d565c5f7527d49f1f885d85e27cadda8ecf37388
Alan Wolfe <alan.wolfe@gmail.com>
2022-07-18 18:02:31 -0700

    begin

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
+/*
+TODO:
+- P-wasserstein where people can be 1 or 2 or whatever.
+- interpolate PDFs
+- measure difference between two pdfs?
+- analytical and tabular or just analytical?
+*/
